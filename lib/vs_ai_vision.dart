f7bd9a899be761fa8875915b0f42079e6f3ad938
Leonardo-sm <leonardo.smichelutti@gmail.com>
2022-08-09 19:57:42 -0300

    ✨ Add dart project

---
+++ lib/vs_ai_vision.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library vs_ai_vision;
+
+export 'src/vs_ai_vision_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
